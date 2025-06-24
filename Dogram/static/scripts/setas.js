de8f51f9be21291f6168e412ecb27ec4a1c04671
João Fellipe Coutinho Athayde <athaydefellipe@gmail.com>
2025-06-24 16:08:31 -0300

    Delete Dogram/static/scripts/setas.js

--- Dogram/static/scripts/setas.js
+++
@@ -1,6 +0,0 @@
-
-const storysContainer = document.getElementById("storys");
-const scrollAmount = 120;
-
-document.querySelector(".arrow.left").onclick = () => {storysContainer.scrollBy({ left: -scrollAmount, behavior: 'smooth' });};
-document.querySelector(".arrow.right").onclick = () => {storysContainer.scrollBy({ left: scrollAmount, behavior: 'smooth' });};
